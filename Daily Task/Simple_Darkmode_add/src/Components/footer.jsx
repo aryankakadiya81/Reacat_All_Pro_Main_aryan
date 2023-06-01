7949ea3ca13ef9c6a0ecf5151bbd7e76c8710578
aryankakadiya81 <aryankakadiya81@gmail.com>
2023-06-01 08:52:23 +0530

    dark_mode_added_app

---
+++ Daily Task/Simple_Darkmode_add/src/Components/footer.jsx
@@ -0,0 +1,66 @@
+function footer() {
+
+    return (
+        <div>
+            <footer className="mainfooter bg-dark text-white" role="contentinfo">
+                <div className="footer-middle">
+                    <div className="container pt-5 mt-5">
+                        <div className="row">
+                            <div className="col-md-4 col-sm-6">
+                                <div className="footer-pad">
+                                    <h4>Heading 1</h4>
+                                    <ul className="list-unstyled">
+                                        <li><a href="#"></a></li>
+                                        <li><a href="#">Payment Center</a></li>
+                                        <li><a href="#">Contact Directory</a></li>
+                                        <li><a href="#">Forms</a></li>
+                                        <li><a href="#">News and Updates</a></li>
+                                        <li><a href="#">FAQs</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6">
+                                <div className="footer-pad">
+                                    <h4>Heading 2</h4>
+                                    <ul className="list-unstyled">
+                                        <li><a href="#">Website Tutorial</a></li>
+                                        <li><a href="#">Accessibility</a></li>
+                                        <li><a href="#">Disclaimer</a></li>
+                                        <li><a href="#">Privacy Policy</a></li>
+                                        <li><a href="#">FAQs</a></li>
+                                        <li><a href="#">Webmaster</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6">
+                                <div className="footer-pad">
+                                    <h4>Heading 3</h4>
+                                    <ul className="list-unstyled">
+                                        <li><a href="#">Parks and Recreation</a></li>
+                                        <li><a href="#">Public Works</a></li>
+                                        <li><a href="#">Police Department</a></li>
+                                        <li><a href="#">Fire</a></li>
+                                        <li><a href="#">Mayor and City Council</a></li>
+                                        <li>
+                                            <a href="#"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12 copy">
+                                <p className="text-center">&copy; Copyright 2018 - Company Name.  All rights reserved.</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+
+export default footer
