dcc2b738876f661d1b194c77a5fbb79bb361520a
aryankakadiya81 <aryankakadiya81@gmail.com>
2023-06-11 19:01:14 +0530

    Update files

--- Vivek Sir tasks/Create React App Using Yarn and add Tailwind and Bootstrap/my-app/src/App.js
+++ Vivek Sir tasks/Create React App Using Yarn and add Tailwind and Bootstrap/my-app/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
     <div className="text-9xl bg-slate-600 h-96">Aryan</div>
+    <div className='fs-5'>Kakadiya</div>
     </>
   );
 }
--- Vivek Sir tasks/Create React App Using Yarn and add Tailwind and Bootstrap/my-app/src/index.js
+++ Vivek Sir tasks/Create React App Using Yarn and add Tailwind and Bootstrap/my-app/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import "jquery"
+import "@popperjs/core/dist/umd/popper"
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap.css"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
