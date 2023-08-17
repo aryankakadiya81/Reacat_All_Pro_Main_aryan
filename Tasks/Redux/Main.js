dd4119e4d7f3d87ac569c521c4a321221cad30dc
aryankakadiya81 <aryankakadiya81@gmail.com>
2023-08-17 13:58:12 +0530

    Redux_Added

---
+++ Tasks/Redux/Main.js
@@ -0,0 +1,51 @@
+let redux = require('redux');
+
+function Cake() {
+    return (
+        {
+            type: "Buy_Cake",
+            info: "Cake_Decre"
+        }
+    )
+}
+
+function Coco() {
+    return (
+        {
+            type: "Buy_Coco",
+            info: "Coco_Decre"
+        }
+    )
+}
+
+let Aryan = (state = { Cakes: 10, Cocos: 20 }, action) => {
+    switch (action.type) {
+        case "Buy_Cake": return ({
+            ...state,
+            Cakes: state.Cakes - 1
+        })
+        case "Buy_Coco": return ({
+            ...state,
+            Cocos: state.Cocos - 1
+        })
+        default: return (state)
+    }
+}
+
+
+let Store = redux.createStore(Aryan);
+console.log("Initial", Store.getState());
+
+let Func = Store.subscribe(
+    () => {
+        console.log("Update", Store.getState())
+    }
+);
+
+Store.dispatch(Cake());
+Store.dispatch(Cake());
+Store.dispatch(Coco());
+
+
+
+// Func();
---
+++ Tasks/Redux/Multiple.js
@@ -0,0 +1,66 @@
+let redux = require('redux');
+
+function Cake() {
+    return (
+        {
+            type: "Buy_Cake",
+            info: "Cake_Decre"
+        }
+    )
+}
+
+function Coco() {
+    return (
+        {
+            type: "Buy_Coco",
+            info: "Coco_Decre"
+        }
+    )
+}
+
+let CakeStore = (state = { Cakes: 10}, action) => {
+    switch (action.type) {
+        case "Buy_Cake": return ({
+            ...state,
+            Cakes: state.Cakes - 1
+        })
+        
+        default: return (state)
+    }
+}
+
+
+let CocoStore = (state = {Cocos: 20 }, action) => {
+    switch (action.type) {
+        case "Buy_Coco": return ({
+            ...state,
+            Cocos: state.Cocos - 1
+        })
+        default: return (state)
+    }
+}
+
+let Aryan = redux.combineReducers({
+    Cake:CakeStore,
+    Coco:CocoStore
+})
+let Store = redux.createStore(Aryan);
+
+console.log("Initial", Store.getState());
+
+let Func = Store.subscribe(
+    () => {
+        console.log("Update", Store.getState())
+    }
+);
+
+Store.dispatch(Cake());
+Store.dispatch(Coco());
+
+Store.dispatch(Cake());
+
+Store.dispatch(Coco());
+
+
+
+// Func();
